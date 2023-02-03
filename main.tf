c1dbc9b938669e0a0f1e9fa47f833f867c567730
maksymrzd <maksym.razdobarov@gmail.com>
2023-02-04 00:09:11 +0200

    Edited main.tf file

--- main.tf
+++ main.tf
@@ -1,3 +1,53 @@
 provider "aws" {
   region = "eu-central-1"
 }
+
+resource "aws_s3_bucket" "websitehosting" {
+  bucket = "tictactoewebpagehosting"
+}
+
+resource "aws_s3_bucket_website_configuration" "websitehostingconf" {
+  bucket = aws_s3_bucket.websitehosting.bucket
+  index_document {
+    suffix = "index.html"
+  }
+  error_document {
+    key = "error.html"
+  }
+}
+
+resource "aws_s3_bucket_policy" "policy" {
+  bucket = aws_s3_bucket.websitehosting.id
+  policy = file("policy.json")
+}
+
+resource "aws_s3_bucket_acl" "acl" {
+  bucket = aws_s3_bucket.websitehosting.id
+
+  acl = "public-read"
+}
+
+
+
+
+resource "aws_s3_object" "index_html" {
+  bucket       = aws_s3_bucket.websitehosting.bucket
+  key          = "index.html"
+  source       = "index.html"
+  content_type = "text/html"
+}
+
+resource "aws_s3_object" "myscripts_js" {
+  bucket       = aws_s3_bucket.websitehosting.bucket
+  key          = "myscripts.js"
+  source       = "myscripts.js"
+  content_type = "application/javascript"
+}
+
+resource "aws_s3_object" "styles_css" {
+  bucket       = aws_s3_bucket.websitehosting.bucket
+  key          = "styles.css"
+  source       = "styles.css"
+  content_type = "text/css"
+}
+
